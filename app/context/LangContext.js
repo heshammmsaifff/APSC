6d79519a9511022ac28c7aa5394434d6c81c2612
Hesham Saif <heshamsaif856@gmail.com>
2025-10-05 16:59:55 +0300

    add new feature to lang(device lang by default)

--- app/context/LangContext.js
+++ app/context/LangContext.js
@@ -1,11 +1,21 @@
 "use client";
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useState, useEffect } from "react";
 
 const LangContext = createContext();
 
 export function LangProvider({ children }) {
   const [lang, setLang] = useState("ar");
   const toggleLang = () => setLang((prev) => (prev === "ar" ? "en" : "ar"));
+
+  useEffect(() => {
+    // نفحص لغة المتصفح
+    const browserLang = navigator.language || navigator.userLanguage;
+    if (browserLang.startsWith("en")) {
+      setLang("en");
+    } else {
+      setLang("ar");
+    }
+  }, []);
 
   return (
     <LangContext.Provider value={{ lang, toggleLang }}>
